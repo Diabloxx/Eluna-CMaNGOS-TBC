59c960dda3edc9cadb211fbae922e80598cd682e
Diabloxx <diabloxx50@gmail.com>
2024-05-10 21:57:20 +0200

    Revert "Fix HP issue"
    This reverts commit be7a6ae32d2852f9b5c912d09bbf3b55e26e718c.

--- src/game/Entities/Creature.cpp
+++ src/game/Entities/Creature.cpp
@@ -548,8 +548,6 @@ bool Creature::UpdateEntry(uint32 Entry, const CreatureData* data /*=nullptr*/, 
                 SetPower(POWER_MANA, curmana);
             }
         }
-        else
-            SetHealthPercent(100.f);
     }
 
     uint32 faction = GetCreatureInfo()->Faction;
